ada4686c18d14f7bbb616651762f74684010706a
Mateo Chavez <113551958+mat212121@users.noreply.github.com>
2022-09-16 20:01:53 +0000

    slice

--- main.py
+++ main.py
@@ -72,15 +72,16 @@ print(' '.join(word_list))
 
 text1 = "Especially in electronic communications, writing in all caps is equivalent to yelling."
 print(text1.upper())
+
 #substrings
 # Take every third character starting from the ninth to the end of the sentence, and print the result.
 
 text2 = "Never trust a computer you can't throw out a window"
-
+print(text2[8::3])
 # Reverses the position of all the characters in the following sentence and displays the result on the screen.
 
 text3 = "It's great to work with computers. They don't argue, they remember everything and they don't drink your beer"
-
+print(text3[::-1])
 # Extract the first word of the following sentence using slicing, and display it on the screen:
 "Controlling complexity is the essence of programming"
 
